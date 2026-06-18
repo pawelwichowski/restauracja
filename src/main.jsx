3a34e1968ef66548b3af06dce15a07b7df2465ca
pawelwichowski <161590855+pawelwichowski@users.noreply.github.com>
2026-06-18 22:37:40 +0000

    Enable immediate form validation

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./authValidation.js";
 import App from "./App.jsx";
 import "./styles.css";
 import "./auth.css";
