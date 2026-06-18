6ccddd321544c84bc570b4d6b028b9220fea52c4
pawelwichowski <161590855+pawelwichowski@users.noreply.github.com>
2026-06-18 22:13:36 +0000

    Expose nearby restaurants endpoint

--- backend/restaurants/urls.py
+++ backend/restaurants/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import restaurant_list
+from .views import nearby_restaurant_list, restaurant_list
 
 urlpatterns = [
     path("restaurants", restaurant_list, name="restaurant-list"),
+    path("restaurants/nearby", nearby_restaurant_list, name="nearby-restaurant-list"),
 ]
