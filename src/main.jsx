1085cf2c7942c3c7a1ae643fc20d75d157b2630c
pawelwichowski <161590855+pawelwichowski@users.noreply.github.com>
2026-06-19 10:17:52 +0000

    Remove dynamic hero title fitting import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./authValidation.js";
-import "./fitHeroTitle.js";
 import App from "./App.jsx";
 import "./styles.css";
 import "./auth.css";
